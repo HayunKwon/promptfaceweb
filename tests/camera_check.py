025750ecccaad40a6ebbb9c8bbfc0fbc879dfc42
M1nu0x0 <oxygen3112@daum.net>
2024-05-21 19:57:35 +0900

    fix camera

--- tests/camera_check.py
+++ tests/camera_check.py
@@ -2,11 +2,12 @@ import cv2
 
 cap = cv2.VideoCapture(0)
 
-has_frame, img = cap.read()
+has_frame, _ = cap.read()
 if not has_frame:
     raise ValueError('Invalid camera')
 
 while True:
+    _, img = cap.read()
     cv2.imshow('img', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
